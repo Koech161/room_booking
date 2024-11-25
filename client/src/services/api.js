22ab6897b073fe0b7e4d06c06b1463963ea619e0
Abel Koech <koechabel161@gmail.com>
2024-11-25 20:23:01 +0300

    update endpoint

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL:"http://127.0.0.1:5555",
+    baseURL:"https://room-booking-2-clfx.onrender.com/",
 });
 
 // Set default headers
